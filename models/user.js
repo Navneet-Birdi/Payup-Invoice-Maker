2143afcf4a66b3e626e0f62d7c5b1652d2c08d4a
Jake Roberts <jake.shaun.roberts1994@hotmail.com>
2022-08-16 21:24:34 +0800

    updated user modal and ddb with user password

--- models/user.js
+++ models/user.js
@@ -10,19 +10,43 @@ User.init(
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+      }
     },
     name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isAlpha: true,
+
+      }
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isEmail: true,
+        isUppercase: false,
+        
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true,
+        min: 8,
+        max: 50,
+      }
     },
     address: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true, 
+      }
     }
   },
   {
