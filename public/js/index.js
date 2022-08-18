24564d66268420c10727a6cef13fa1ea076e1c3c
Navneet Kaur <nbirdi@yahoo.com>
2022-08-18 18:35:48 +0800

    invoice file done

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,9 @@
 // Get the modal
-const modal = document.getElementById('id01');
+//const modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  // if (event.target == modal) {
-  //   modal.style.display = "none";
+//window.onclick = function(event  // i(event.target == modal) {
+  //  odal.style.display = "none";
   // }
-}
+  //}
+//}
