b5d313e8baffd0712dbd82b92aa37b9fe7de83a4
Jake Roberts <jake.shaun.roberts1994@hotmail.com>
2022-08-18 20:02:53 +0800

    updated modals, handlebars and db

---
+++ models/invoice.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Invoice extends Model {}
+
+Invoice.init(
+  {
+    // Manually define the primary key
+    order_number: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    product: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    product_quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    in_stock: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    // Prevent sequelize from renaming the table
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Invoice'
+  }
+);
+
+module.exports = Invoice;
