8e7111ce4fe1b694d892392eea078ae29eccb1d6
mleftwich <mleftwich@live.com>
2022-08-27 11:22:18 +0800

    config file

--- config/connection.js
+++ config/connection.js
@@ -2,27 +2,26 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
   {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
+  "development": {
+    "username": process.env.DB_user,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "port": process.env.DB_PORT,
+    "dialect": "mysql"
   },
-
- {
-   "mysqlOptions": {
-     "authProtocol": "default"
-   },
-   "previewLimit": 50,
-   "server": "localhost",
-   "port": 3306,
-   "driver": "MySQL",
-   "name": "payup_connection",
-   "database": "payup_db",
-   "username": "root",
-   "password": "root"
-})
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+  }
+}
+)
 
 module.exports = sequelize;
