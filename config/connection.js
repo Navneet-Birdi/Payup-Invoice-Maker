38cc8d53c02cd416e54a4565b25635976c68721e
mleftwich <mleftwich@live.com>
2022-08-27 11:39:39 +0800

    connection

--- config/connection.js
+++ config/connection.js
@@ -3,25 +3,8 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(
   {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "port": process.env.DB_PORT,
+   "use_env_variable": "JAWSDB_URL",
     dialect: "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    dialect: "mysql"
-  },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    dialect: "mysql"
-  }
-}
-)
+  })
 
 module.exports = sequelize;
